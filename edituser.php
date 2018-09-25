9eb76f6050898a27c3eb2b72828a8cbbc2227e6d
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-09-25 10:45:01 -0400

    Add files via upload

--- edituser.php
+++ edituser.php
@@ -16,12 +16,16 @@ if (isset($_GET['id']) && $_GET['edit']=="edit") {
   echo "<form action=\"\" method=\"post\">";
 
   while ($row = $result->fetch_assoc()) {
-    echo "<input type=\"text\" disabled value=\"" . $row['userid'] . "\">";
+    echo "<input name=\"userid\" type=\"text\" disabled value=\"" . $row['userid'] . "\">";
     echo "<br />";
-    echo "<input type=\"text\" disabled value=\"" . $row['username'] . "\">";
+    echo "<input name= \"username\" type=\"text\" disabled value=\"" . $row['username'] . "\">";
     echo "<br />";
-    echo "<input type=\"text\" disabled value=\"" . $row['password'] . "\">";
+    echo "<input name= \"password\" type=\"text\" disabled value=\"" . $row['password'] . "\">";
+    echo "<input type=\"submit\" name=\"submit\" value=\"change\">";
+    echo "<br />";
   }
+
+echo "</form>";
 
 } else {
   echo "Something went wrong.";
