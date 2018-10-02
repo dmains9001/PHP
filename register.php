5ed7c9299e6fccb29bc2a3f56106c287c12a4f64
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-02 10:46:21 -0400

    Register sanitize username

--- register.php
+++ register.php
@@ -1,7 +1,18 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   require('dbconnection.php');
+  // Grab POST data - Could be DANGEROUS, due to XSS or SQL injection
   $username = $_POST['username'];
+    //Sanitize the username ($username) by removing tags
+    $username = filter_var($username, FILTER_SANITIZE_STRING);
+      // Trim whitespace from beginning and end of $username
+      $username = trim($username);
+        //Remove slashes from $username; no / allowed
+        $username = stripslashes($username);
+          //Remove white space from middle of string
+          $username = str_replace(' ', '',$username); // First param = string to look at, 2nd removes spaces, 3rd replaces spaces with nothing
+          
+  //Grab POST data - Password will be hashed so no need to sanitize
   $password = $_POST['password'];
   $password = password_hash($password, PASSWORD_BCRYPT);
   $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
