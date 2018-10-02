f14024c5f9435f10636df9eea7b02e60ae967fd0
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-02 10:09:28 -0400

    Test

--- navbar.php
+++ navbar.php
@@ -3,5 +3,5 @@
 <a href=register.php>Register</a> |
 <a href=login.php>Login</a> |
 
-<?php if($_SERVER["PHP_SELF"]=='users.php' {<strong>Users</strong>});
-?>
+<?php echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href='users.php'> <strong>Users</strong></a>":
+"<a href='users.php'>Users</a>"; ?>
