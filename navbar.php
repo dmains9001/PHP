99460ea06ef8420b8581a7e6b5b2acc70c2894bc
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-02 10:16:41 -0400

    Test

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <?php echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href='users.php'> <strong>Users</strong></a>":
-"<a href='users.php'>Users</a>"; ?>
+"<a href='users.php'>Users</a>"; ?> |
 <?php echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<a href='upload.php'> <strong>Upload</strong></a>":
-"<a href='upload.php'>Upload</a>"; ?>
+"<a href='upload.php'>Upload</a>"; ?> |
 <?php echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<a href='register.php'> <strong>Register</strong></a>":
-"<a href='register.php'>Register</a>"; ?>
+"<a href='register.php'>Register</a>"; ?> |
 <?php echo (basename($_SERVER['PHP_SELF']) == "login.php") ? "<a href='login.php'> <strong>Login</strong></a>":
-"<a href='login.php'>Login</a>"; ?>
+"<a href='login.php'>Login</a>"; ?> |
