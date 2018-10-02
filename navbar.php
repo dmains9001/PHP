4aeab4a6e1a0408db6f0623bb2b787e1b8ca7549
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-02 09:27:33 -0400

    navbar update

--- navbar.php
+++ navbar.php
@@ -1,2 +1,16 @@
+echo "<br />"";
+echo "<a href=users.php>Users</a>";
+echo "<a href=upload.php>Upload</a>";
+echo "<a href=register.php>Register</a>";
+echo "<a href=login.php>Login</a>";
+
+
+}
 <?php echo (basename($_SERVER['PHP_SELF']) == "users.php") ? "<a href='users.php'> <strong>Users</strong></a>":
 "<a href='users.php'>Users</a>";
+echo (basename($_SERVER['PHP_SELF']) == "login.php") ? "<a href='users.php'> <strong>Login</strong></a>":
+"<a href='login.php'>Login</a>";
+echo (basename($_SERVER['PHP_SELF']) == "register.php") ? "<a href='users.php'> <strong>Register</strong></a>":
+"<a href='register.php'>Register</a>";
+echo (basename($_SERVER['PHP_SELF']) == "upload.php") ? "<a href='upload.php'> <strong>Upload</strong></a>":
+"<a href='upload.php'>Upload</a>";
