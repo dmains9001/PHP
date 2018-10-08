e6a2a71274cb7eb89fe45c678121cb4e8f62fdb5
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-08 11:19:52 -0400

    test

--- cookies.php
+++ cookies.php
@@ -6,13 +6,13 @@ if (isset($_COOKIE)['lastvisit']))
 {
   $notification = "You have been here recently";
   $lastvisit = $_COOKIE['lastvisit'];
-  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/")
+  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 }
 else {
   {
     $notification = "You haven't been here recently";
     $lastvisit = $_COOKIE['lastvisit'];
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/")
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
   }
 }
 //setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
@@ -38,6 +38,20 @@ else {
         echo "This is your first visit.";
         setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
         //setcookie($cookie_name, $cookie_value, time() + (60), "/");
+      }
+
+      if(isset($_COOKIE['user']))
+      {
+      $last = $_COOKIE['user'];
+      }
+      $year = 31536000 + time();
+
+      setcookie($bob, time(), $year) ;
+      if (isset($bob))
+      {
+        $change = time () - $last;
+        if ( $change > 86400)
+        echo "You last visited on ". date("m/d/y",$last);
       }
 
     ?>
