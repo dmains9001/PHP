d24ac31ebf9747cf7a094dddf3882beff119bdd0
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-08 11:07:45 -0400

    Timestamp

--- cookies.php
+++ cookies.php
@@ -23,20 +23,20 @@ $cookie_value = "bob";
       {
         echo "This is your first visit.";
         //setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
-        //setcookie($cookie_name, $cookie_value, time() + (60), "/");
+        setcookie($cookie_name, $cookie_value, time() + (60), "/");
       }
-      if(isset($_COOKIE['lastvisit']))
-      {
-      $last = $_COOKIE['lastvisit'];
-      }
-      $year = 31536000 + time();
+      //if(isset($_COOKIE['lastvisit']))
+      //{
+      //$last = $_COOKIE['lastvisit'];
+      //}
+      //$year = 31536000 + time();
 
-      setcookie($lastvisit, time (), $year) ;
-      if (isset ($last))
-      {
-        $change = time () - $last;
-        if ( $change > 86400)
-        echo "You last visited on ". date("m/d/y",$last);
+      //setcookie($lastvisit, time (), $year) ;
+      //if (isset ($last))
+      //{
+      //  $change = time () - $last;
+      //  if ( $change > 86400)
+      //  echo "You last visited on ". date("m/d/y",$last);
 
     ?>
   </body>
