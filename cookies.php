af8191ed50748f5d994c0f8d7efe9cbbb9d8c697
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-08 11:27:41 -0400

    test

--- cookies.php
+++ cookies.php
@@ -1,57 +1,22 @@
 <?php
-$cookie_name = "lastvisit";
-$cookie_value = "bob";
+if(isset($_COOKIE['lastvisit']))
+{
+$visit = $_COOKIE['lastvisit']; }
+$year = 31536000 + time() ;
 
-//if (isset($_COOKIE)['lastvisit']))
-//{
-//  $notification = "You have been here recently";
-//  $lastvisit = $_COOKIE['lastvisit'];
-//  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-//}
-//else {
-//    $notification = "You haven't been here recently";
-//    $lastvisit = $_COOKIE['lastvisit'];
-//    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-//}
-//setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
-//86400 seconds = 1 day
+setcookie(lastvisit, time (), $year) ;
+if (isset ($visit))
+{
+$change = time () - $visit;
+if ( $change > 86400)
+{
+echo "You were last here on ". date("m/d/y",$visit) ;
 
+}
+}
+else
+{
+echo "This is your first time here";
+
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-      if(isset($_COOKIE['user']))
-      {
-        echo "You have been here before.";
-      }
-
-      else
-      {
-        echo "This is your first visit.";
-        setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
-        //setcookie($cookie_name, $cookie_value, time() + (60), "/");
-      }
-
-      if(isset($_COOKIE['user']))
-      {
-      $last = $_COOKIE['user'];
-      }
-      $year = 31536000 + time();
-
-      setcookie($bob, time(), $year) ;
-      if (isset($bob))
-      {
-        $change = time () - $last;
-        if ( $change > 86400)
-        echo "You last visited on ". date("m/d/y",$last);
-      }
-
-    ?>
-  </body>
-</html>
