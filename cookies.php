069206daa4d48c76c4f489fad47339632530037f
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-08 11:33:44 -0400

    test

--- cookies.php
+++ cookies.php
@@ -1,27 +1,57 @@
 <?php
-if(isset($_COOKIE['AboutVisit']))
-{
-$last = $_COOKIE['AboutVisit']; }
-$year = 31536000 + time() ;
-//this adds one year to the current time, for the cookie expiration
-setcookie(AboutVisit, time (), $year) ;
-if (isset ($last))
-{
-$change = time () - $last;
-if ( $change > 86400)
-{
-echo "Welcome back! <br> You last visited on ". date("m/d/y",$last) ;
-// Tells the user when they last visited if it was over a day ago
-}
-else
-{
-echo "Thanks for using our site!";
-//Gives the user a message if they are visiting again in the same day
-}
-}
-else
-{
-echo "Welcome to our site!";
-//Greets a first time user
-}
+$cookie_name = "lastvisit";
+$cookie_value = "bob";
+
+//if (isset($_COOKIE)['lastvisit']))
+//{
+//  $notification = "You have been here recently";
+//  $lastvisit = $_COOKIE['lastvisit'];
+//  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+//}
+//else {
+//    $notification = "You haven't been here recently";
+//    $lastvisit = $_COOKIE['lastvisit'];
+//    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+//}
+//setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
+//86400 seconds = 1 day
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      if(isset($_COOKIE['user']))
+      {
+        echo "You have been here before.";
+      }
+
+      else
+      {
+        echo "This is your first visit.";
+        setcookie($cookie_name, $cookie_value, time() + 86400 * 30, "/");
+        //setcookie($cookie_name, $cookie_value, time() + (60), "/");
+      }
+
+      if(isset($_COOKIE['user']))
+      {
+      $last = $_COOKIE['user'];
+      }
+      $year = 31536000 + time();
+
+      setcookie($bob, time(), $year) ;
+      if (isset($bob))
+      {
+        $change = time () - $last;
+        if ( $change > 86400)
+        echo "You last visited on ". date("m/d/y",$last);
+      }
+
+    ?>
+  </body>
+</html>
