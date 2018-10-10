a4e6889364d5069408573b15b941971d96a360d0
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-10 11:24:35 -0400

    testfolder

---
+++ testfolder.php
@@ -0,0 +1,10 @@
+<?php
+$testfolder = ' /var/www/html/damian/PHP/test';
+
+  if (file_exists($testfolder)) {
+    echo "The folder $testfolder exists upon this material plane";
+}
+  else {
+    echo "The folder $testfolder is lost to the void";
+}
+?>
