759698893868349c003efddb6f82ea0772c99263
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-10 11:43:27 -0400

    testfolder

--- testfolder.php
+++ testfolder.php
@@ -3,6 +3,13 @@ $testfolder = '/var/www/html/damian/PHP/test';
 
   if (file_exists($testfolder)) {
     echo "The folder $testfolder exists upon this material plane";
+      $foldertest = is_dir("test");
+        if ($foldertest) {
+          echo "Test is a folder for the storage of lesser files.";
+        }
+        else {
+          echo "Test is a lesser file. Pathetic.";
+        }
 }
   else {
     echo "The folder $testfolder is lost to the void; we shall bring it to be. Umbasa.";
