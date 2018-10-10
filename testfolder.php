f1beabc6156d1946b3c635cbfc66ed3118e3a683
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-10 11:45:25 -0400

    testfolder

--- testfolder.php
+++ testfolder.php
@@ -2,7 +2,7 @@
 $testfolder = '/var/www/html/damian/PHP/test';
 
   if (file_exists($testfolder)) {
-    echo "The folder $testfolder exists upon this material plane";
+    echo "The folder $testfolder exists upon this material plane.";
       $foldertest = is_dir("test");
         if ($foldertest) {
           echo " Test is a folder for the storage of lesser files.";
