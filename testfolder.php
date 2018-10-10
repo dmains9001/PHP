601b61a2b9669e99e3dbb53f0be21cb40b6cad3c
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-10 11:30:04 -0400

    testfolder

--- testfolder.php
+++ testfolder.php
@@ -1,10 +1,13 @@
 <?php
 $testfolder = '/var/www/html/damian/PHP/test';
+$create = exec('mkdir test');
 
   if (file_exists($testfolder)) {
     echo "The folder $testfolder exists upon this material plane";
 }
   else {
-    echo "The folder $testfolder is lost to the void";
+    echo "The folder $testfolder is lost to the void; we shall bring it to be. Umbasa.";
+
 }
+
 ?>
