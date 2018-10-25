24b6860d17d15fac3487fd446193f3139c3dba54
dmains9001 <dmains9001@gmail.com>
2018-10-25 03:19:03 -0400

    test

--- Reference/edituser.php
+++ Reference/edituser.php
@@ -5,33 +5,48 @@ if (!isset($_SESSION)){
 
 if (!isset($_SESSION['username'])){
   header('Location: login.php');
+}
 
-  // require('navbar.php'); < ---- Adds in the navbar
+// Check to see if there is any post info...it would come from the change form
+if (isset($_POST['submit'])){
+  require('dbconnection.php'); //bring in database connection
+  $userid = $_POST['userid'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $sql = "UPDATE users set username = \"$username\", password = \"$password\" WHERE userid = $userid";
+  $result = $conn->query($sql);
+  var_dump($result);
+  header('Location: users.php');
 }
 
 if (isset($_GET['id']) && $_GET['edit']=="edit") {
-  require('dbconnection.php'); //Brings in database connection
-  $sql = "SELECT * from users where userid = " . $_GET['id']; //"id" is int datatype; don't quote "" it.
+  require('dbconnection.php'); //bring in database connection
+  $sql = "SELECT * from users where userid = " . $_GET['id']; //id is int datatype don't quote it
   $result = $conn->query($sql);
 
-  //Starts the form
-  echo "<form action=\"\" method=\"post\">";
+require('navbar.php');
+
+  echo "<form action=\"?userid=\" method=\"post\">";
 
   while ($row = $result->fetch_assoc()) {
-    echo "<input name=\"userid\" type=\"text\" disabled value=\"" . $row['userid'] . "\">";
+    echo "<input type=\"text\" disabled value=\"" . $row['userid'] . "\">";
+    echo "<input name=\"userid\" type=\"hidden\" value=\"" . $row['userid'] . "\">";
     echo "<br />";
     echo "<input name=\"username\" type=\"text\" value=\"" . $row['username'] . "\">";
     echo "<br />";
     echo "<input name=\"password\" type=\"text\" value=\"" . $row['password'] . "\">";
-    echo "<input type=\"submit\" name=\"submit\" value=\"change\">";
     echo "<br />";
+    echo "<input type=\"submit\" name=\"submit\" value=\"change\">";
   }
 
 echo "</form>";
 
+
 } else {
-  echo "Something went wrong.";
+echo "You should not be here.";
 }
+
+
 
 
 
