30055b1d743124a4571a7ce06744eb19871f1284
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-10-25 13:02:18 -0400

    1

--- FollowMe/examples/editprofile.php
+++ FollowMe/examples/editprofile.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   $title = $_POST['title'];
   $description = $_POST['description'];
 
-  $sql = "UPDATE fm_users SET (first_name,last_name,title,description) VALUES ('$firstname','$lastname','$title','$description',) where email = '$email'";
+  $sql = "UPDATE fm_users SET (first_name = '$firstname',last_name = '$lastname', title = '$title', description = '$description') where email = '$email'";
   $conn->query($sql);
 
   if ($conn) {
