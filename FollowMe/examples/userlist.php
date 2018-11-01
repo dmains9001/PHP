183b5f591b5330a5d2f0cd96143bb2d6c6682714
dmains9001 <42810880+dmains9001@users.noreply.github.com>
2018-11-01 09:43:42 -0400

    Ternary

--- FollowMe/examples/userlist.php
+++ FollowMe/examples/userlist.php
@@ -127,7 +127,7 @@ while($row = $following_result->fetch_assoc()) {
                           <div class=\"col-md-3 col-sm-2\">
           									<div class=\"form-check\">
                           	<label class=\"form-check-label\">
-                          		<input class=\"form-check-input\" type=\"checkbox\" value=\"\" (in_array($row['user_id'] == $following_id)) ? 'echo \"checked\"' : 'echo \"unchecked\"';>
+                          		<input class=\"form-check-input\" type=\"checkbox\" value = (in_array($row['user_id'], $following_id) ? 'echo \"checked\"' : 'echo \"unchecked\"';>
                           		<span class=\"form-check-sign\"></span>
                           	</label>
                         </div>
@@ -136,6 +136,7 @@ while($row = $following_result->fetch_assoc()) {
                 	</li>";
 										}
 
+										//if (in_array($row['user_id'], $following_id)){echo "checked";}
 										//Link the fm_follows columns (followed_by and followed_user) to fm_users as foreign keys, so
 										//they're linked to userid in fm_users, so it auto-populates the fm_follows tables with the
 										//userid values.
